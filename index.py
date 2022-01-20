0eb667460ef91ff6d3910ef2a24f0be965c0c676
Tyler Long <tyler4long@gmail.com>
2022-01-20 10:57:55 -0800

    First working version

--- index.py
+++ index.py
@@ -5,5 +5,8 @@ sdk = SDK(os.environ['RINGCENTRAL_CLIENT_ID'], os.environ['RINGCENTRAL_CLIENT_SE
 platform = sdk.platform()
 platform.login(os.environ['RINGCENTRAL_USERNAME'], os.environ['RINGCENTRAL_EXTENSION'], os.environ['RINGCENTRAL_PASSWORD'])
 
-res = platform.get('/restapi/v1.0/account/~/extension/~')
-print(res.json().extensionNumber)
+
+r = platform.get('/restapi/v1.0/account/~/extension/~/profile-image')
+f = open('temp.png', 'wb')
+f.write(r.response().content)
+f.close()
